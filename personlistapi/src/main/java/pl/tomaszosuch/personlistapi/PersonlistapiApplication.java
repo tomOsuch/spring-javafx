bf05c57a2224c6a27b4e366a926d2d82ccbb955f
t.osuch <Tom@sz180288>
2022-06-03 15:16:36 +0200

    first commit Person_List_Api

---
+++ personlistapi/src/main/java/pl/tomaszosuch/personlistapi/PersonlistapiApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszosuch.personlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonlistapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonlistapiApplication.class, args);
+    }
+
+}
